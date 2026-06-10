bcc183f3363c14889451c32324c708a26aaa6915
Rafael45ACP <rafaelgriego1911@gmail.com>
2026-06-10 22:50:24 +0000

    added betting functionality

--- Blackjack/jack.js
+++ Blackjack/jack.js
@@ -9,4 +9,71 @@ let playerCards = document.getElementById("playerCards");
 let playerValue = document.getElementById("playerValue");
 let Pmoney = document.getElementById("money");
 
+let fDiv = document.getElementById("fDiv");
+
 Pmoney.textContent = money;
+
+if(bet === 0) {
+    let betInput = document.createElement("input");
+    let betBTN = document.createElement("button");
+    betBTN.textContent = "Place Bet!";
+    betInput.type = "number";
+
+    let betBR = document.createElement("br");
+
+    let betSpan = document.createElement("span");
+    betSpan.textContent = bet;
+    
+    let betValue = 0;
+    
+    
+    betInput.addEventListener('keydown', function (event) {
+        
+        if (event.key === 'Enter') {
+        betValue = betInput.value.trim();
+            if(betValue > money){
+                alert("Bet cannot exceed starting money!");
+                betInput.value = '';
+            }
+            else if(betValue < money || betValue == money)
+            {
+                bet = Number(betValue);
+                betSpan.textContent = bet;
+                return;
+            }
+        }
+        if (
+            event.key === 'Backspace' ||
+            event.key === 'Delete' ||
+            event.key === 'ArrowLeft' ||
+            event.key === 'ArrowRight' ||
+            event.key === 'Tab'
+        ) return;
+
+        if (!/^\d$/.test(event.key)) {
+            event.preventDefault();
+        }
+        
+    });
+    
+    betBTN.addEventListener('click',function(){
+        
+        betValue = betInput.value.trim();
+            if(betValue > money){
+                alert("Bet cannot exceed starting money!");
+                betInput.value = '';
+            }
+            else if(betValue < money || betValue == money)
+            {
+                bet = Number(betValue);
+                betSpan.textContent = bet;
+                return;
+            }
+    });
+    
+        fDiv.appendChild(betInput);
+        fDiv.appendChild(betBTN);
+        fDiv.appendChild(betBR);
+        fDiv.appendChild(betSpan);
+
+}
